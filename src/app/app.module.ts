931f6fe39fa0f6430501a60a1cc35c74ad0373f5
Adria87 <adriamanes@gmail.com>
2021-10-27 00:41:12 +0200

    Hilo de binance abriendo y cerrando en funcion de los seleccionados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,7 +13,8 @@ import { ToastModule } from 'primeng/toast';
 import { PanelModule } from 'primeng/panel';
 import { ListboxModule } from 'primeng/listbox';
 import { MessageService } from 'primeng/api';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { CardModule } from 'primeng/card';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TradePanelComponent } from './components/binance/trade-panel/trade-panel.component';
 
 @NgModule({
@@ -36,6 +37,7 @@ import { TradePanelComponent } from './components/binance/trade-panel/trade-pane
     PanelModule,
     ListboxModule,
     BrowserModule,
+    CardModule,
     BrowserAnimationsModule,
   ],
   providers: [MessageService],
--- src/app/components/binance/trade-panel/trade-panel.component.ts
+++ src/app/components/binance/trade-panel/trade-panel.component.ts
@@ -5,7 +5,8 @@ import { BinanceApiService } from "src/app/services/binance-api.service";
 import { TradePanelService } from "src/app/services/trade-panel.service";
 import { Symbol } from "src/app/models/binance/exchange-info"
 import { mapTo } from 'rxjs/operators';
-import { merge, Observable, forkJoin } from "rxjs";
+import { merge, Observable, forkJoin, Subscription } from "rxjs";
+import { webSocket, WebSocketSubject } from "rxjs/webSocket";
 @Component({
   selector: 'app-trade-panel',
   templateUrl: './trade-panel.component.html',
@@ -23,6 +24,9 @@ export class TradePanelComponent implements OnInit {
 
   filteredSymbols: Symbol[] = [];
   filteredSelectedSymbols: Symbol[] = [];
+
+  tickerPriceMap = new Map<String, String>();
+  webSocketMap = new Map<String, Subscription>();
   // submitted = false;
 
   constructor(
@@ -51,8 +55,24 @@ export class TradePanelComponent implements OnInit {
       this.selectedFilterAssets = p.peticionBackend.selectedFilterAssets as string[];
       this.refrescarFilteredSymbolsArray(this.selectedFilterAssets);
       this.filteredSelectedSymbols = p.peticionBackend.filteredSelectedSymbols as Symbol[];
+      this.filteredSelectedSymbols.forEach(fss => {
+        this.tickerPriceMap.set(fss.symbol!, '');
+        const socket = webSocket(`wss://stream.binance.com:9443/ws/${fss.symbol?.toLowerCase()}@ticker`).subscribe(
+          (msg: any) => {
+            // console.log('message received: ' + JSON.stringify(msg.c))
+            this.tickerPriceMap.set(fss.symbol!, msg.c);
+          }, // Called whenever there is a message from the server.
+          err => console.log(err), // Called if at any point WebSocket API signals some kind of error.
+          () => console.log('complete') // Called when connection is closed (for whatever reason).
+        );
+        this.webSocketMap.set(fss.symbol!, socket);
+      });
     });
 
+  }
+
+  getTickerPrice(symbol: any) {
+    return this.tickerPriceMap!.get(symbol) != undefined ? this.tickerPriceMap!.get(symbol)! : '';
   }
 
   refrescarFilteredSymbols(event: any) {
@@ -61,8 +81,44 @@ export class TradePanelComponent implements OnInit {
   }
 
   refrescarFilteredSymbolsArray(seleccionados : string[]){
+    this.filteredSelectedSymbols.forEach(s => {
+      this.webSocketMap.get(s.symbol!)?.unsubscribe();
+    });
     this.filteredSymbols = this.totalAssets.filter(e => seleccionados.includes(e.baseAsset as string) || seleccionados.includes(e.quoteAsset as string));
     this.filteredSelectedSymbols = [];
+    this.webSocketMap.clear();
+    this.tickerPriceMap.clear();
+  }
+
+  refrescarWebSockets(event: any) {
+    if (event.value.length == 0){
+      this.webSocketMap.forEach(e => {
+        e.unsubscribe();
+      })
+      this.webSocketMap.clear();
+      this.tickerPriceMap.clear();
+    }
+    
+    (event.value as Symbol[]).forEach((v: any) => {
+      if (!this.webSocketMap.has(v.symbol)){
+        const socket = webSocket(`wss://stream.binance.com:9443/ws/${v.symbol?.toLowerCase()}@ticker`).subscribe(
+          (msg: any) => {
+            // console.log('message received: ' + JSON.stringify(msg.c))
+            this.tickerPriceMap.set(v.symbol!, msg.c);
+          }, // Called whenever there is a message from the server.
+          err => console.log(err), // Called if at any point WebSocket API signals some kind of error.
+          () => console.log('complete') // Called when connection is closed (for whatever reason).
+        );
+        this.webSocketMap.set(v.symbol!, socket);
+      }
+    });
+
+    this.webSocketMap.forEach((element, key) => {
+      if (!event.value.map((e:Symbol) => e.symbol).includes(key)){
+        this.webSocketMap.get(key)?.unsubscribe();
+        this.webSocketMap.delete(key);
+      }
+    });
   }
 
   saveTradePanel(): void {
